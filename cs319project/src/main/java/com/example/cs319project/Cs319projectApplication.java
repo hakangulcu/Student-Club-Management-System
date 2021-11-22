7c0a1dc3777cbc62b196164e73c8db1a2a71b4aa
Berk Baltaci <berk.baltaci@ug.bilkent.edu.tr>
2021-11-22 21:16:17 +0300

    Implementation database start

---
+++ cs319project/src/main/java/com/example/cs319project/Cs319projectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs319project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs319projectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs319projectApplication.class, args);
+    }
+
+}
---
+++ cs319project/src/test/java/com/example/cs319project/Cs319projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs319project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs319projectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
