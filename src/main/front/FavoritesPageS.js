e7288bff4061e75ade1aefc9247d78b03da0da45
Umut Ege Özdemir <88161164+umutozdemr@users.noreply.github.com>
2021-12-25 15:08:25 +0300

    favoritespagejs

---
+++ src/main/front/FavoritesPageS.js
@@ -0,0 +1,35 @@
+
+
+const getData = () => {
+
+
+    axios.get('https://projectdeneme.herokuapp.com/student/getFavoriteClubs/1').then(function (response) {
+
+        console.log(response);
+
+
+
+
+        for(var i = 0; i < response.data.length; i++) {
+
+            var clubname = response.data[i].clubName;
+
+            document.body.innerHTML =' <ul class="list-group list-group-horizontal mt-3 justify-content-evenly" style="background-color: #cbcaca" >\n' +
+                '        <li class="list-group-item col-lg-4 col-sm-4 col-md-4 text-center ms-2" style="border: none;background-color: #cbcaca">' + clubName +'</li>\n' +
+                '        <li class="list-group-item  col-lg-2 ms-2 " style="border: none;background-color: #cbcaca">\n' +
+                '            <button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Leave</button>\n' +
+                '        </li>\n' +
+                '        <li class="list-group-item col-lg-2 ms-2" style="border: none;background-color: #cbcaca">\n' +
+                '            <button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Show</button>\n' +
+                '        </li>\n' +
+                '        <li class="list-group-item col-lg-2 ms-2" style="border: none;background-color: #cbcaca">\n' +
+                '            <button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Remove From Favorite</button>\n' +
+                '        </li>\n' +
+                '    </ul>';
+
+        }
+    })
+};
+
+
+
