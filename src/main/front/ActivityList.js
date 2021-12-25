58c279ab4b87654e7292abfa0154c6e150a54b14
hakangulcu <hakan.gulcu@ug.bilkent.edu.tr>
2021-12-25 23:05:48 +0300

    some minor
    minor

--- src/main/front/ActivityList.js
+++ src/main/front/ActivityList.js
@@ -1,16 +1,12 @@
 
 const tbody = document.getElementById("tbody");
 
-
-
-
 const getData = () => {
 
 
     axios.get('https://projectdeneme.herokuapp.com/clubs/listActivities').then(function (response) {
             var today = new Date();
             console.log(response);
-
 
             for (var i = 0; i < response.data.length; i++) {
                 var activitydesc = response.data[i].activityDescription;
@@ -27,6 +23,37 @@ const getData = () => {
     )
 };
 
+async function putActivityListData(){
+    const res = await axios.put('#', {
 
+    })
+}
+
+/**
+ * async function putActivityData() {
+    var mydate = new Date($('#date').val());
+    console.log(mydate.toDateString());
+    const res = await axios.put('https://projectdeneme.herokuapp.com/activities/addNewActivity', {
+    
+    activityName: activityNameText.value,
+    date: mydate,
+     
+    capacity: quotaText.value,
+    ge250Point: ge250Text.value,
+      
+    //averageRate: guestText.value,
+    place: placeText.value,
+    
+    participantList: null,
+    activityDescription: null
+    
+})        .then(function (response) {
+    console.log(response);
+    
+})
+.catch(function (error) {
+    console.log(error);
+});
+ */
 
 
