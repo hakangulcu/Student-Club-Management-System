54917efcdc014452e9ed0ca6016ae38ba090e05a
Umut Ege Özdemir <88161164+umutozdemr@users.noreply.github.com>
2021-12-25 14:37:18 +0300

    clubdetails page

--- src/main/front/clubdetailspageSaxios.js
+++ src/main/front/clubdetailspageSaxios.js
@@ -1,31 +1,47 @@
 
-const navActivities = document.getElementById("navActivities");
-//const axios = require('axios');
-//https://projectdeneme.herokuapp.com/activities/listActivities
-//const axios = require('axios').default;
+const logotest = document.getElementById("asdas");
+const pastact = document.getElementById("pastact1");
+const upact = document.getElementById("upact1");
+
 
 
 const getData = () => {
-    axios.get('').then(function (response) {
+
+    axios.get('https://projectdeneme.herokuapp.com/clubs/getClub/1').then(function (response) {
 
         console.log(response);
-        var clubname = response.data[i].date;
+        var clubname = response.data.clubName;
+        var clubdesc = response.data.clubTextDescription;
+
         document.getElementById("clubName").textContent=clubname;
-        for(var i = 0; i < response.data.length; i++) {
-            var activityName = response.data[i].activityName;
+        document.getElementById("clubDesc").textContent=clubdesc;
 
-            var clubdesc = response.data[i].place;
-
-            document.body.innerHTML = document.body.innerHTML + '';
-
-        }
     })
         .catch(function (error) {
             // handle error
             console.log(error);
         });
+    axios.get('https://projectdeneme.herokuapp.com/clubs/listActivities/1').then(function (response) {
+        var today = new Date();
+        console.log(response);
+
+
+
+
+        for(var i = 0; i < response.data.length; i++) {
+            var activitydate = response.data[i].date;
+            var activityname = response.data[i].activityName;
+            const line31 = document.createElement('line31');
+            line31.innerHTML ='<u1 class="list-group list-group-horizontal mb-3"><li class="list-group-item col-lg-10 col-sm-2 col-md-2  col-4 "style="border: none"> '+ activityname + '</li><li class="list-group-item col-lg-2 col-sm-2 col-md-2   "style="border: none"><button type="button" class="btn btn-danger btn-outline-dark " style="color: white">Show</button></li></u1>';
+
+            if(activitydate < today) {
+                pastact.appendChild(line31);}
+            else {
+                upact.appendChild(line31); }
+        }
+    })
 };
 
 
 
-navActivities.addEventListener('click', getData)
+logotest.addEventListener('click', getData)
