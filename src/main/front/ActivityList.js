8710360b50e81b25d172238abfbe25cdea1d2549
hakangulcu <hakan.gulcu@ug.bilkent.edu.tr>
2021-12-25 23:17:35 +0300

    fix activity list
    fix some issue

--- src/main/front/ActivityList.js
+++ src/main/front/ActivityList.js
@@ -1,60 +1,40 @@
 
-const tbody2 = document.getElementById("tbody1");
 
-const getData = () => {
+//const axios = require('axios');
+//https://projectdeneme.herokuapp.com/activities/listActivities
+//const axios = require('axios').default;
 
 
-    axios.get('https://projectdeneme.herokuapp.com/clubs/listActivities/1').then(function (response) {
-
-            console.log(response);
-
-            for (var i = 0; i < response.data.length; i++) {
-                var activitydesc = response.data[i].activityDescription;
-                var activityname = response.data[i].activityName;
-                var activityquota = response.data[i].capacity;
-                var activitydate = response.data[i].date;
-                var activityplace = response.data[i].place;
-                const line32 = document.createElement('tbody1');
-                line32.textContent = "";
-                line32.innerHTML += '<tr style="height: 20%;"><td><div className="card"><div style="background-color:LightGray;" className="container1"><h1 className="activity-name">' + activityname + '</h1> <p style="color:black"> ' + activitydesc + '</p> <h1 className="neon">'+ activityquota+' </h1> <h1 className="neon"> '+ activitydate+' </h1> <h1 className="neon2">'+activityplace+'</h1> </div> </div> </td> </tr>';
-                tbody2.appendChild(line32);
-            }
-            ;
-        }
-    )
-};
-
-async function putActivityListData(){
-    const res = await axios.put('#', {
-
-    })
-}
-
-/**
- * async function putActivityData() {
-    var mydate = new Date($('#date').val());
-    console.log(mydate.toDateString());
-    const res = await axios.put('https://projectdeneme.herokuapp.com/activities/addNewActivity', {
-    
-    activityName: activityNameText.value,
-    date: mydate,
-     
-    capacity: quotaText.value,
-    ge250Point: ge250Text.value,
-      
-    //averageRate: guestText.value,
-    place: placeText.value,
-    
-    participantList: null,
-    activityDescription: null
-    
-})        .then(function (response) {
+const getData2 = () => {
+    axios.get('https://projectdeneme.herokuapp.com/activities/listActivities').then(function (response) {
     console.log(response);
-    
-})
-.catch(function (error) {
-    console.log(error);
-});
- */
+        for(var i = 0; i < response.data.length; i++) {
+            var activityName = response.data[i].activityName;
+            var activitydate = response.data[i].date;
+            var activityplace = response.data[i].place;
+            var accap = response.data[i].capacity;
+            var ge250 = response.data[i].ge250Point;
+            document.body.innerHTML = document.body.innerHTML + '<ul class="list-group list-group-horizontal mb-2 mt-4 "  >\n' +
+                '      <li class="list-group-item col-lg-2 col-sm-2 col-md-2 text-center  col-4" id="activityClub1" style="border: none"> </li>\n' +
+                '      <li class="list-group-item col-lg-2 col-sm-2 col-md-2 text-center  col-4" style="border: none"><span id="activityName1"> '+ activityName+ ' </span></li>\n' +
+                '      <li class="list-group-item col-lg-2 col-sm-2 col-md-2 d-none d-sm-block text-center  col-2" style="border: none"><span id="activityDate1"> '+ activitydate+ ' </span></li>\n' +
+                '      <li class="list-group-item col-lg-2 col-sm-2 col-md-2 d-none d-sm-block text-center  col-2" style="border: none"><span id="activityPlace1"> '+ activityplace+ ' </span></li>\n' +
+                '      <li class="list-group-item col-lg-1 col-sm-1 col-md-1 d-none d-sm-block text-center  col-1" style="border: none"><span id="activityQuota1"> '+ accap+ '</span></li>\n' +
+                '      <li class="list-group-item col-lg-1 col-sm-1 col-md-1 d-none d-sm-block text-center  col-1" style="border: none"><span id="activityGe1"> '+ ge250+ ' </span></li>\n' +
+                '      <li class="list-group-item col-lg-2 col-2 justify-content-center d-none  d-sm-block" style="border: none">\n' +
+                '        <button id = "joinButton" type="button" class="btn btn-danger btn-outline-dark justify-content-center" style="color: white">Join</button>\n' +
+                '      </li>';
 
+        }
+    })
+    .catch(function (error) {
+    // handle error
+    console.log(error);
+    });
+};
+ 
+/**
+const currentData = await axios.get('https://projectdeneme.herokuapp.com/activities/listActivities')then(res => res.data);
+joinButton.addEventListener('click', getData)
+*/
 
