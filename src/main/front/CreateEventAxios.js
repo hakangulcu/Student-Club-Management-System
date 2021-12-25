bba5ec7824debc5f746e1a7172a92379dc69591c
hakangulcu <hakan.gulcu@ug.bilkent.edu.tr>
2021-12-25 17:41:13 +0300

    createEvent axios
    axios update

--- src/main/front/CreateEventAxios.js
+++ src/main/front/CreateEventAxios.js
@@ -1,58 +1,80 @@
-const textarea = document.getElementById('textarea');
+
 const activityNameText = document.getElementById('activityNameText');
 const descriptionText = document.getElementById('descriptionText');
 const guestText = document.getElementById('guestText');
-const datepicker = document.getElementById('datepicker');
+const datepicker = document.getElementById('date');
 const quotaText = document.getElementById('quotaText');
 const placeText = document.getElementById('placeText');
 const ge250Text = document.getElementById('ge250Text');
 const saveButton = document.getElementById('saveButton');
-const asdsadsa = document.getElementById('saveButton');
-
+ /** 
 const getDataCreateActivity = () => {
     console.log("salkjdsakjdsakj");
     axios.get('https://projectdeneme.herokuapp.com/activities/addNewActivity').then(function (response) {
+        console.log(response);
+
+    }
+
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+};
+*/
+async function putActivityData() {
+    console.log($('#date').val());
+    const res = await axios.put('https://projectdeneme.herokuapp.com/activities/addNewActivity', {
+    
+    activityName: activityNameText.value,
+    date: $('#date').val(),
+     
+    capacity: quotaText.value,
+    ge250Point: ge250Text.value,
+      
+    //averageRate: guestText.value,
+    place: placeText.value,
+    
+    participantList: null,
+    activityDescription: null
+    
+})        .then(function (response) {
     console.log(response);
     
-    })
-    
-    .catch(function (error) {
-    // handle error
+})
+.catch(function (error) {
     console.log(error);
-    });
+});
+/** 
+const res2 = await axios.put('http://localhost:8080/clubs/addActivityToClub/1/1', {
+
+});
+*/
+//http://localhost:8080/clubs/addActivityToClub/1/1
+
+ /**   axios.put('https://projectdeneme.herokuapp.com/activities/addNewActivity', {
+        activityName: activityNameText.value,
+        date: datepicker.value,
+        capacity: quotaText.value,
+        ge250Point:ge250Text.value,
+        averageRate:guestText.value,
+        place:placeText.value,
+        organizerClubList: null,
+        participantList:null,
+        activityDescription: null,
+        evaluationList:null,
+        commentList:null,
+        schedule:null
+    })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    */ 
 };
 
 
-const sendDataCreateActivity = () => {
-    axios
-      .put(
-        'https://projectdeneme.herokuapp.com/activities/addNewActivity',
-        { 
-          activityName: activityNameText.value,
-          date: datepicker.value,
-          capacity: quotaText.value,
-          ge250Point: ge250Text.value,
-          averageRate: "",
-          place: placeText.value,
-          
-          // password: 'pistol'
-          //activityName :
-        },
-        {
-          // headers: {
-          //   'Content-Type': 'application/json'
-          // }
-        }
-      )
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        console.log(err, err.response);
-      });
-  };
 
 
-asdsadsa.addEventListener('click', getDataCreateActivity);
-saveButton.addEventListener('click', sendDataCreateActivity);
-asdsadsa.addEventListener('click', getDataCreateActivity);
+saveButton.addEventListener('click', putActivityData);
