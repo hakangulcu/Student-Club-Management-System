a0bed3db5d7ab00cf6aacbfec754f90db00a92db
Umut Ege Özdemir <88161164+umutozdemr@users.noreply.github.com>
2021-12-26 03:47:42 +0300

    bigbrain time

--- src/main/front/ActivityParticipantList.js
+++ src/main/front/ActivityParticipantList.js
@@ -1,13 +1,14 @@
 
 const tbody1 = document.getElementById("tbody");
 
-
+const id= sessionStorage.getItem("clubId");
+console.log(id);
 
 
 const getData = () => {
 
 
-    axios.get('https://projectdeneme.herokuapp.com/activities/getParticipantList/1').then(function (response) {
+    axios.get('https://projectdeneme.herokuapp.com/activities/getParticipantList/'+id).then(function (response) {
 
             console.log(response);
 
@@ -17,7 +18,7 @@ const getData = () => {
                 var surname = response.data[i].surname;
                 var sid = response.data[i].userId;
                 var email = response.data[i].email;
-                var dep = response.data[i].email;
+                var dep = response.data[i].department;
                 const line31 = document.createElement('line31');
                 line31.innerHTML += '<tr><th scope="col "style="color:white;">'+ name+'</th><th scope="col"> '+surname+'</th> <th scope="col">'+sid+'</th> <th scope="col">'+ email +'</th> <th scope="col">'+dep+'</th> <td style="text-align: center;"><button class="remove">Remove from Activity</button></td> </tr>';
                     tbody1.appendChild(line31);
--- src/main/front/MyActivityDetails.js
+++ src/main/front/MyActivityDetails.js
@@ -1,18 +1,21 @@
 const tbody = document.getElementById("tbody");
+const id= sessionStorage.getItem("clubId");
+console.log(id);
 const getData = () => {
-    axios.get('https://projectdeneme.herokuapp.com/clubs/listActivities/1').then(function (response) {
+    axios.get('https://projectdeneme.herokuapp.com/clubs/listActivities/'+id).then(function (response) {
         console.log(response);
         for(var i = 0; i < response.data.length; i++) {
             var actName = response.data[i].activityName;
             var actQuota = response.data[i].capacity;
             var activityDesc = response.data[i].activityDescription;
             var date1 = response.data[i].date;
+            var activityId = response.data[i].id;
 
             for(var i = 0; i < response.data.length; i++) {
 
                 var clubname = response.data[i].clubName;
                 const line31 = document.createElement('line31');
-                line31.innerHTML +=' <tr style="height: 20%;"> <td style="width: 75%;"> <div class="card"> <div class="container1"> <h1 class="activity-name"><span id="activityname"> activityname'+actName+'</span></h1> <p><span id="descr">description'+activityDesc+' </span></p> <h1 class="neon"><span id="quota">Quota : 27 / 55'+ actQuota+'</span> </h1> <h1 class="neon"><span id="date">date'+ date1 +'</span> </h1> </div><div class="container"><div class="row" style="text-align: center;"><button class="hidden-btn" onclick=""> Customize Event</button></div><div class="row" style="text-align: center;"><button class="hidden-btn2"> Delete Event</button></div><div class="row" style="text-align: center;"><button class="hidden-btn" onclick="";> Show Participant</button></div></div></div></td></tr>';
+                line31.innerHTML +=' <tr style="height: 20%;"> <td style="width: 75%;"> <div class="card"> <div class="container1"> <h1 class="activity-name"><span id="activityname"> activityname'+actName+'</span></h1> <p><span id="descr">description'+activityDesc+' </span></p> <h1 class="neon"><span id="quota">Quota : 27 / 55'+ actQuota+'</span> </h1> <h1 class="neon"><span id="date">date'+ date1 +'</span> </h1> </div><div class="container"><div class="row" style="text-align: center;"><button class="hidden-btn" onclick=""> Customize Event</button></div><div class="row" style="text-align: center;"><button class="hidden-btn2"> Delete Event</button></div><div class="row" style="text-align: center;"><button class="hidden-btn" onclick="goDetails('+ activityId +')";> Show Participant</button></div></div></div></td></tr>';
                 tbody.appendChild(line31);
             }
 
@@ -25,3 +28,8 @@ const getData = () => {
         });
 
 };
+function goDetails(activityId) {
+    var aId = activityId;
+    sessionStorage.setItem("activityId", aId );
+    document.location.href = "ActivityParticipantList.html";
+}
