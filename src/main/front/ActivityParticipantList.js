a9769c9da9d70a192e85670091b31dc4eb59c311
Umut Ege Özdemir <88161164+umutozdemr@users.noreply.github.com>
2021-12-26 04:11:32 +0300

    Update ActivityParticipantList.js

--- src/main/front/ActivityParticipantList.js
+++ src/main/front/ActivityParticipantList.js
@@ -1,7 +1,7 @@
 
 const tbody1 = document.getElementById("tbody");
 
-const id= sessionStorage.getItem("clubId");
+const id= sessionStorage.getItem("activityId");
 console.log(id);
 
 
@@ -20,7 +20,7 @@ const getData = () => {
                 var email = response.data[i].email;
                 var dep = response.data[i].department;
                 const line31 = document.createElement('line31');
-                line31.innerHTML += '<tr><th scope="col "style="color:white;">'+ name+'</th><th scope="col"> '+surname+'</th> <th scope="col">'+sid+'</th> <th scope="col">'+ email +'</th> <th scope="col">'+dep+'</th> <td style="text-align: center;"><button class="remove">Remove from Activity</button></td> </tr>';
+                line31.innerHTML += '<tr><th scope="col "style="color:white;">'+ name+'</th><th scope="col"> '+surname+'</th> <th scope="col">'+sid+'</th> <th scope="col">'+ email +'</th> <th scope="col">'+dep+'</th> <td style="text-align: center;"><button class="remove" onclick="remove('+ response.data[i].id+' )">Remove from Activity</button></td> </tr>';
                     tbody1.appendChild(line31);
             }
             ;
@@ -28,6 +28,16 @@ const getData = () => {
     )
 };
 
+async function remove(idst)
+{
+    const res = await axios.put('https://projectdeneme.herokuapp.com/students/deleteActivityFromStudent/'+ idst + '/'+ id)
+        .then(function (response) {
+        console.log(response);
 
+    })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
 
